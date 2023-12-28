1986a53ec489d5eff01a8d1e283ff1efbd19be2a
kolazsys <kolazsys@users.noreply.github.com>
2023-12-28 11:38:00 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Centrex\Wallet\Tests;
 
+use Centrex\Wallet\WalletServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Centrex\Wallet\WalletServiceProvider;
 
 class TestCase extends Orchestra
 {
