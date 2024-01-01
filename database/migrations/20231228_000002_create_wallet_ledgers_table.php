c6604a34c127c5402e2d33c2ac8777082d4cdd21
rochi88 <rochi88@users.noreply.github.com>
2024-01-01 08:48:28 +0000

    Fix styling

--- database/migrations/20231228_000002_create_wallet_ledgers_table.php
+++ database/migrations/20231228_000002_create_wallet_ledgers_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 use Centrex\Wallet\Models\Wallet;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
--- src/Contracts/WalletTransaction.php
+++ src/Contracts/WalletTransaction.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Centrex\Wallet\Contracts;
 
 interface WalletTransaction
